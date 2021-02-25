56b4f1bbe75dbbc0c6e166d66236e3723b2000a0
Femi Ayodeji <femidotayodeji@gmail.com>
2021-02-25 19:50:07 +0100

    Modify blocks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector('.container');
-for(var i = 0; i <= 100; i++){
+for(var i = 0; i <= 50; i++){
     const block = document.createElement('div');
     block.classList.add('block');
     container.appendChild(block);
@@ -9,13 +9,13 @@ function animateBlocks(){
     anime({
         targets: '.block',
         translateX: function(){
-            return anime.random(-700,700);
+            return anime.random(-0,700);
         },
         translateY: function(){
             return anime.random(-500,500);
         },
         scale: function(){
-            return anime.random(1,5)
+            return anime.random(1,3)
         },
         easing: 'linear',
         duration: 3000,
